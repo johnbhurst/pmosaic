e62719be6c15db97ef46909c6635feea92dbbc24
John Hurst <john.b.hurst@gmail.com>
2024-08-01 19:40:52 +1200

    refactor: Moved common routines into pmosaic module; simplified.

--- bin/mklib.py
+++ bin/mklib.py
@@ -15,50 +15,16 @@ parser.add_argument("-f", "--libfile", default="libfile.txt", help="File to stor
 parser.add_argument("files", nargs="+", help="Image files to process.")
 args = parser.parse_args()
 
-
-def average_rgb(image):
-    pixels = image.load()
-    width, height = image.size
-    r = g = b = 0
-    for x in range(width):
-        for y in range(height):
-            r += pixels[x, y][0]
-            g += pixels[x, y][1]
-            b += pixels[x, y][2]
-    size = width * height
-    return {"red": r // size, "green": g // size, "blue": b // size}
-
-
-def average_quadrants(image):
-    width, height = image.size
-    half_width = width // 2
-    half_height = height // 2
-    q1 = image.crop((0, 0, half_width, half_height))
-    q2 = image.crop((half_width, 0, width, half_height))
-    q3 = image.crop((0, half_height, half_width, height))
-    q4 = image.crop((half_width, half_height, width, height))
-    return {
-        "top_left": average_rgb(q1),
-        "top_right": average_rgb(q2),
-        "bot_left": average_rgb(q3),
-        "bot_right": average_rgb(q4)
-    }
-
-
-def create_mosaic_library(files, libfile):
-    with open(libfile, "w") as f:
-        for i, filename in enumerate(files):
-            logging.info(f"Processing {filename}")
-            image = Image.open(filename)
+pmosaic.setup_logging(level=logging.DEBUG if args.debug else logging.INFO)
+with open(args.libfile, "w") as f:
+    for i, filename in enumerate(args.files):
+        logging.info(f"Processing {filename}")
+        with Image.open(filename) as image:
             # crop the image to the middle square portion
             width, height = image.size
             if width > height:
                 image = image.crop(((width - height) // 2, 0, (width + height) // 2, height))
             elif height > width:
                 image = image.crop((0, (height - width) // 2, width, (height + width) // 2))
-            quadrant_colors = average_quadrants(image)
+            quadrant_colors = pmosaic.average_quadrants(image)
             print(json.dumps({"filename": filename, "quadrant_colors": quadrant_colors}), file=f)
-
-
-pmosaic.setup_logging(level=logging.DEBUG if args.debug else logging.INFO)
-create_mosaic_library(args.files, args.libfile)
--- bin/pmosaic.py
+++ bin/pmosaic.py
@@ -24,5 +24,34 @@ def setup_logging(level=logging.INFO):
     logger.addHandler(file_handler)
 
 
+def average_rgb(image):
+    pixels = image.load()
+    width, height = image.size
+    r = g = b = 0
+    for x in range(width):
+        for y in range(height):
+            r += pixels[x, y][0]
+            g += pixels[x, y][1]
+            b += pixels[x, y][2]
+    size = width * height
+    return {"red": r // size, "green": g // size, "blue": b // size}
+
+
+def average_quadrants(image):
+    width, height = image.size
+    half_width = width // 2
+    half_height = height // 2
+    q1 = image.crop((0, 0, half_width, half_height))
+    q2 = image.crop((half_width, 0, width, half_height))
+    q3 = image.crop((0, half_height, half_width, height))
+    q4 = image.crop((half_width, half_height, width, height))
+    return {
+        "top_left": average_rgb(q1),
+        "top_right": average_rgb(q2),
+        "bot_left": average_rgb(q3),
+        "bot_right": average_rgb(q4)
+    }
+
+
 
 
