833ac44e6be30596338f4bbac2a2ba9b24c526d9
John Hurst <john.b.hurst@gmail.com>
2024-08-04 09:56:49 +1200

    fix: Removed commented-out lines.

--- bin/pmosaic.py
+++ bin/pmosaic.py
@@ -60,22 +60,6 @@ def load_library(libfile):
 
 
 def quadrant_colors_distance(qc1, qc2):
-    # v1 = pow(float(qc1["top_left"]["red"] - qc2["top_left"]["red"]), 2.0)
-    # v2 = pow(float(qc1["top_left"]["green"] - qc2["top_left"]["green"]), 2.0)
-    # v3 = pow(float(qc1["top_left"]["blue"] - qc2["top_left"]["blue"]), 2.0)
-    # v4 = pow(float(qc1["top_right"]["red"] - qc2["top_right"]["red"]), 2.0)
-    # v5 = pow(float(qc1["top_right"]["green"] - qc2["top_right"]["green"]), 2.0)
-    # v6 = pow(float(qc1["top_right"]["blue"] - qc2["top_right"]["blue"]), 2.0)
-    # v7 = pow(float(qc1["bot_left"]["red"] - qc2["bot_left"]["red"]), 2.0)
-    # v8 = pow(float(qc1["bot_left"]["green"] - qc2["bot_left"]["green"]), 2.0)
-    # v9 = pow(float(qc1["bot_left"]["blue"] - qc2["bot_left"]["blue"]), 2.0)
-    # v10 = pow(float(qc1["bot_right"]["red"] - qc2["bot_right"]["red"]), 2.0)
-    # v11 = pow(float(qc1["bot_right"]["green"] - qc2["bot_right"]["green"]), 2.0)
-    # v12 = pow(float(qc1["bot_right"]["blue"] - qc2["bot_right"]["blue"]), 2.0)
-    # sum = v1 + v2 + v3 + v4 + v5 + v6 + v7 + v8 + v9 + v10 + v11 + v12
-    # print(f"{v1=}, {v2=}, {v3=}, {v4=}, {v5=}, {v6=}, {v7=}, {v8=}, {v9=}, {v10=}, {v11=}, {v12=}")
-    # print(sum)
-    # return sqrt(sum)
     return sqrt(float(sum(
         pow(qc1[quadrant][color]^2 - qc2[quadrant][color], 2)
         for quadrant in ["top_left", "top_right", "bot_left", "bot_right"]
