fix: Changed average_rgb(), average_quadrants() to use array processing for mean instead of counting each pixel individually.

bin/pmosaic.py
@@ -6,6 +6,7 @@ import datetime
 import inspect
 import json
 import logging
+import numpy as np
 import os
 from math import sqrt
 
@@ -35,32 +36,21 @@ def crop_square(image):
         return image
 
 
-def average_rgb(image):
-    pixels = image.load()
-    width, height = image.size
-    r = g = b = 0
-    for x in range(width):
-        for y in range(height):
-            r += pixels[x, y][0]
-            g += pixels[x, y][1]
-            b += pixels[x, y][2]
-    size = width * height
-    return {"red": r // size, "green": g // size, "blue": b // size}
+def average_rgb(data):
+    r, g, b = data.mean(axis=(0, 1))
+    return {"red": int(r), "green": int(g), "blue": int(b)}
 
 
 def average_quadrants(image):
-    width, height = image.size
-    half_width = width // 2
-    half_height = height // 2
-    q1 = image.crop((0, 0, half_width, half_height))
-    q2 = image.crop((half_width, 0, width, half_height))
-    q3 = image.crop((0, half_height, half_width, height))
-    q4 = image.crop((half_width, half_height, width, height))
+    data = np.array(image)
+    height, width, _ = data.shape
+    height_half, width_half = height // 2, width // 2
+
     return {
-        "top_left": average_rgb(q1),
-        "top_right": average_rgb(q2),
-        "bot_left": average_rgb(q3),
-        "bot_right": average_rgb(q4)
+        "top_left": av_rgb(data[:height_half, :width_half]),
+        "top_right": av_rgb(data[:height_half, width_half:]),
+        "bot-Left": av_rgb(data[height_half:, :width_half]),
+        "bot_right": av_rgb(data[height_half:, width_half:])
     }
 
 